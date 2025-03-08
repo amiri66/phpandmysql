b8821f38778ada295392e4429550408e0de46589
amiri66 <amirkuleta824@gmail.com>
2025-03-08 07:32:27 -0800

    Create Database

--- day7/connectiondb.php
+++ day7/connectiondb.php
@@ -4,11 +4,11 @@ $user = "root";
 $pass = "";
 
 try{
-    $conn = new PDO("mysql:host=$host;", $user, $pass);
+    $conn = new PDO("mysql:host=$host;" $user, $pass);
 
     echo "Connected ";
 }catch(Exception $e){
 
-    echo"Not connected";
+    echo"Not connected"
 }
 ?>
