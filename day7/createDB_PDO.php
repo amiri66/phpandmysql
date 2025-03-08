3b862d383c5e87505f97a25567bceac9b2050da8
amiri66 <amirkuleta824@gmail.com>
2025-03-08 07:29:47 -0800

    Database Creation

---
+++ day7/createDB_PDO.php
@@ -0,0 +1,17 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+
+try{
+    $conn = new PDO("mysql:host=$host;", $user, $pass);
+
+    $sql = "CREATE DATABASE test123";
+
+    $conn->exec($sql);
+
+    echo "Database created";
+}catch(Exception $e){
+
+    echo"Somthing went wrong!";
+}
