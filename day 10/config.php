dffcffbb6aad59a035751fd91206fcd247883883
amiri66 <amirkuleta824@gmail.com>
2025-05-03 07:41:15 -0700

    amiri

---
+++ day 10/config.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+    $server = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'amiri';
+
+
+    try{
+        $connect = new PDO("mysql:host=$server;dbname=$dbname", $username, $password);
+
+
+    }catch(Exeption $e){
+        echo "Something went wrong";
+    }
